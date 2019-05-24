34d787de129d3ac7bbb4eb0a10596bda2ae9a193
Roman Marchenko <marchenko.roman.s@gmail.com>
2019-05-24 21:03:09 +0700

    add loading spinner

---
+++ src/components/LoadingSpinner/LoadingSpinner.tsx
@@ -0,0 +1,5 @@
+import * as React from 'react';
+import { ScaleLoader } from 'react-spinners';
+import "./loadingSpinner.scss";
+
+export default (props: any) => <div className="scaleLoaderContainer"><ScaleLoader color="rgba(103, 58, 183, 1)" /></div>

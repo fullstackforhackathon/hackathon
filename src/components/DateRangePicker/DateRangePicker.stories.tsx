1e91d0dc048c7e80267afba29468fff20734de0c
Roman Marchenko <marchenko.roman.s@gmail.com>
2019-05-24 21:09:28 +0700

    added story for DateRangePicker

---
+++ src/components/DateRangePicker/DateRangePicker.stories.tsx
@@ -0,0 +1,6 @@
+import * as React from 'react';
+import { storiesOf } from '@storybook/react';
+import DateRangePicker from './DateRangePicker';
+
+storiesOf('DateRangePicker', module)
+    .add('default', () => <DateRangePicker />)

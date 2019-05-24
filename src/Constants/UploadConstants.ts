fb07e5ae0b5715b5334e8330738246b06e7c5cfc
Roman Marchenko <marchenko.roman.s@gmail.com>
2019-05-24 17:24:16 +0700

    move constant generator to utils

--- src/Constants/UploadConstants.ts
+++ src/Constants/UploadConstants.ts
@@ -1,3 +1,3 @@
-import { constantGenerator } from "./ConstantGenerator";
+import { constantGenerator } from "../utils/ConstantGenerator";
 // USE crc snippet to this: write "crc" -> push tab -> enter action type name -> press tab to uppercase;
 export const { UPLOAD_FILE, UPLOAD_FILE_PENDING, UPLOAD_FILE_FULFILLED, UPLOAD_FILE_REJECTED } = constantGenerator("UPLOAD_FILE");
